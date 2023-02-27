0dde789746c51014d6e61e4a39f1bdd662963f89
arch-herobrine <112319383+arch-herobrine@users.noreply.github.com>
2023-02-27 23:19:38 +0000

    aaa

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 //変数s
-const express=require("express"),app = express(), ytdl = require("ytdl-core"),cors = require('cors'),{Buffer}=require("buffer")
+const express=require("express"),app = express(), ytdl = require("ytdl-core"),cors = require('cors'),{Buffer}=require("buffer"),fs=require("fs")
 app.use(cors())
 app.use(express.raw({type:"*/*"}))
+
+//エラーハンドリング
+process.on("uncaughtException", function(err) {
+    console.log(err);
+});
 
 app.get("/",(req,res)=>{
     res.send('<a href="/youtube/">youtube proxyはこちら</a>')
@@ -12,29 +17,26 @@ app.get("/youtube", (req, res) => {
     <p><input type="submit" value="GO"></p>
   </form>`)
 })
-app.post("/youtube",(req,res)=>{
+app.post("/youtube",async(req,res)=>{
     
         
     console.log(Buffer.from(req.body).toString())
     res.redirect(`/youtube/${ytdl.getURLVideoID(decodeURIComponent(Buffer.from(req.body).toString()).split(";")[0].replace("url=",""))}`)
 })
-app.get("/youtube/:id", (req, res) => {
+app.get("/youtube/:id", async(req, res) => {
     var r = { id: req.params.id }
-    var stream = ytdl(req.params.id);
-    stream.on('info', (info) => {
-        r.title = info.title
-        console.log(r)
+    var stream = await ytdl.getBasicInfo(req.params.id);
+  fs.writeFile("log",JSON.stringify(stream,null,"  "),()=>{})
+      var title=stream.videoDetails.title
+        r.title = title
         res.render("../views/video.ejs",r)
-        stream.destroy()
-    })
+        
+  
 })
 app.get("/yt/v/:id", (req, res) => {
     var URL = req.params.id;
-    console.log("url", URL);
     var stream = ytdl(URL);
     stream.on('info', (info) => {
-        console.log(info.title);
-        console.log(info.video_id);
         res.header('Content-Disposition', 'attachment; filename="video.mp4"');
         ytdl(URL, {
             format: 'mp4'
